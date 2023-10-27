0de94e36f333a1e54a7177b9926fd08b14bbd391
Axelmarting <axelmarting2002@gmail.com>
2023-10-27 16:55:52 -0300

    Agregando altura a cuadros

---
+++ funciones.js
@@ -0,0 +1,11 @@
+function mostrarResultadoCalorias() {
+    // Aquí puedes escribir el código que se ejecutará cuando se haga clic en el botón
+    alert("Se hizo clic en el botón calorias");
+}
+
+
+
+function mostrarResultadoPeso() {
+    // Aquí puedes escribir el código que se ejecutará cuando se haga clic en el botón
+    alert("Se hizo clic en el botón peso");
+}
