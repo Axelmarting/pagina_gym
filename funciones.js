ac41c02ca87d1c11011faa831c2902803898b307
Axelmarting <axelmarting2002@gmail.com>
2023-10-27 17:45:01 -0300

    Recien empezando las funciones

--- funciones.js
+++ funciones.js
@@ -1,11 +1,59 @@
 function mostrarResultadoCalorias() {
-    // Aquí puedes escribir el código que se ejecutará cuando se haga clic en el botón
-    alert("Se hizo clic en el botón calorias");
+    let pesoIngresado;
+    let alturaIngresada;
+    let aniosIngresados;
+    let masculoChequeado;
+    let femeninoChequeado;
+    let sexo;
+
+    pesoIngresado = parseFloat(document.getElementById("peso1").value);
+    alturaIngresada = parseFloat(document.getElementById("altura1").value);
+    aniosIngresados = parseFloat(document.getElementById("anios1").value);
+
+    femeninoChequeado = document.getElementById("femenino1");
+    masculoChequeado = document.getElementById("masculino1");
+
+    if (femeninoChequeado.checked) {
+        sexo = "f";
+    } else if (masculoChequeado.checked) {
+        sexo = "m";
+    } else {
+        alert("Por favor, seleccione un sexo.");
+    }
+
+    switch(sexo){
+        case "f":
+            break;
+
+        default:
+            break;
+    }
+
 }
 
 
 
 function mostrarResultadoPeso() {
-    // Aquí puedes escribir el código que se ejecutará cuando se haga clic en el botón
-    alert("Se hizo clic en el botón peso");
+    let pesoIngresado;
+    let alturaIngresada;
+    let aniosIngresados;
+    let masculoChequeado;
+    let femeninoChequeado;
+    let sexo;
+
+    pesoIngresado = parseFloat(document.getElementById("peso2").value);
+    alturaIngresada = parseFloat(document.getElementById("altura2").value);
+    aniosIngresados = parseFloat(document.getElementById("anios1").value);
+
+    femeninoChequeado = document.getElementById("femenino2");
+    masculoChequeado = document.getElementById("masculino2");
+
+    if (femeninoChequeado.checked) {
+        sexo = "f";
+    } else if (masculoChequeado.checked) {
+        sexo = "m";
+    } else {
+        alert("Por favor, seleccione un sexo.");
+    }
+
 }
